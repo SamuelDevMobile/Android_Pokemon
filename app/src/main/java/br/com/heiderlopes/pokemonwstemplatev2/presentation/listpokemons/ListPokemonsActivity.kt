f152faf149359bda400f0d94e683b86f2ee90078
Logon Aluno <logonrmlocal@fiap.com.br>
2022-12-01 22:40:18 -0300

    implementacao na activity

--- app/src/main/java/br/com/heiderlopes/pokemonwstemplatev2/presentation/listpokemons/ListPokemonsActivity.kt
+++ app/src/main/java/br/com/heiderlopes/pokemonwstemplatev2/presentation/listpokemons/ListPokemonsActivity.kt
@@ -2,11 +2,30 @@ package br.com.heiderlopes.pokemonwstemplatev2.presentation.listpokemons
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import br.com.heiderlopes.pokemonwstemplatev2.R
+import br.com.heiderlopes.pokemonwstemplatev2.domain.model.ViewState
+import org.koin.android.ext.android.inject
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class ListPokemonsActivity : AppCompatActivity() {
+
+    private val listPokemonsViewModel: ListPokemonsViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_pokemons)
+        listPokemonsViewModel.getPokemons()
+        registerObservers()
+    }
+
+    private fun registerObservers() {
+        listPokemonsViewModel.pokemonResult.observe(this) {
+            when(it){
+                is ViewState.Failure -> Log.d("POKEMON_API", "DEU RUIM")
+                is ViewState.Success -> Log.d("POKEMON_API", it.data[0].name)
+                is ViewState.Loading -> Log.d("POKEMON_API", "CARREGANDO")
+            }
+        }
     }
 }
--- app/src/main/java/br/com/heiderlopes/pokemonwstemplatev2/presentation/main/MainActivity.kt
+++ app/src/main/java/br/com/heiderlopes/pokemonwstemplatev2/presentation/main/MainActivity.kt
@@ -1,12 +1,24 @@
 package br.com.heiderlopes.pokemonwstemplatev2.presentation.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import br.com.heiderlopes.pokemonwstemplatev2.R
+import br.com.heiderlopes.pokemonwstemplatev2.presentation.listpokemons.ListPokemonsActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btPokemonList = findViewById<Button>(R.id.btPokemonList)
+
+        btPokemonList.setOnClickListener {
+            val listPokemonsIntent = Intent(this, ListPokemonsActivity::class.java)
+            startActivity(listPokemonsIntent)
+        }
+
     }
 }
